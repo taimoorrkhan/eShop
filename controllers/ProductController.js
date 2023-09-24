7faef2b37126180e8bca6ef005e15219b4352aaa
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-09-24 11:35:46 +0500

    added Api features

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -2,9 +2,13 @@ const Product = require('../models/product')
 
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const APIFeatures = require("../utils/apiFeatures");
+
+
 //Creating Product
 
 const AddProduct = async (req, res, next) => {
+
 
   const product = await Product.create(req.body);
   res.status(201).json({
@@ -17,7 +21,13 @@ const AddProduct = async (req, res, next) => {
 
 //Get All Products
 const getProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find();
+  const resPerPage = 4;
+  const productCount = await Product.countDocuments();
+  const apiFeatures = new APIFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resPerPage)
+  const products = await apiFeatures.query;
   res.status(200).json({
     success: true,
     count: products.length,
